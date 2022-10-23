644af3db275665c141abc6505bb80a06e2884ec8
JCMpirata <113981043+JCMpirata@users.noreply.github.com>
2022-10-23 16:42:20 +0000

    Segunda parte apuntes

--- main.py
+++ main.py
@@ -85,6 +85,79 @@ if color1 == color2:
 else:
   print("Son distintos colores")
 
+print("\n")
 
+#Crea una lista con la frase, en la que cada letra y espacio es un elemento de la lista
+lista = list("Python is awesome")
 
+# Mostrar la lista
+print("lista = ", lista)
+# acceso a un miembro de la lista (get)
+print("lista[4] = ", lista[4])
+print("lista[-3] = ", lista[-3])
+
+# extracción de sub-lista
+#Del 0 incluido al 5 incluido, sin incluir el 6
+print("lista[:6] = ", lista[:6])
+#Del 7 incluido hasta el 9 incluido, sin incluir el 10
+print("lista[7:10] = ", lista[7:10])
+#Del 10 incluido hasta el final
+print("lista[10:] = ", lista[10:])
+# A partir del 2 va sumando de 5 en 5 hasta el final (2, 7, 12...)
+print("lista[2::5] = ", lista[2::5])
+#A partir del -3 va restando de 6 en 6 hasta el final (-3, -9, -15)
+print("lista[-3::-6] = ", lista[-3::-6])
+
+# Escritura de un miembro de la lista (set)
+#Inserta en el puesto 11 una letra b
+print("""lista[11] = "b" """)
+lista[11] = "b"
+print("lista = ", lista)
+#Inserta en puesto 13 y 14 las letras fg, sin incluir el puesto 15
+print("""lista[13:15] = "fg" """)
+lista[13:15] = "fg"
+print("lista = ", lista)
+
+# Eliminar un miembro de la lista (del)
+# Elimina el elemento del puesto 15
+print("""del lista[15]""")
+del lista[15]
+print("lista = ", lista)
+# Elimina la y de la lista
+print("""lista.remove("y")""")
+lista.remove("y")
+print("lista = ", lista)
+# Mientras que haya espacios en la lista, borrar los espacios
+print("""while " " in lista: lista.remove(" ")""")
+while " " in lista:
+    lista.remove(" ")
+print("lista = ", lista)
+# Elimina los elementos de la lista del 0 incluido hasta 6 incluido, sin incluir el 7
+print("""del lista[:7]""")
+del lista[:7]
+print("lista = ", lista)
+
+# Eliminar el último elemento (y devolverlo)
+print("""lista.pop() -> Devuelve :""", lista.pop())
+print("lista = ", lista)
+
+# Agregar un elemento al final de la lista
+print("""lista.append("h")""")
+lista.append("h")
+print("lista = ", lista)
+
+# Agregar un elemento en cualquier lugar de la lista
+print("""lista.insert(2, "d")""")
+lista.insert(2, "d")
+print("lista = ", lista)
+
+print("""lista.insert(2, "c")""")
+lista.insert(2, "c")
+print("lista = ", lista)
+
+# Concatenación de una lista
+#Añade al final de la lista tantos elementos como quieras
+print("""lista.extend(["i", "j"])""")
+lista.extend(["i", "j"])
+print("lista = ", lista)
 
